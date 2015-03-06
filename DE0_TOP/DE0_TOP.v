a44a7dc2b1d8585726f3f4e186637c44bc3c88f1
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-03-06 14:18:01 -0500

    Pared down the DE0_TOP for use in EECS 301

--- DE0_TOP/DE0_TOP.v
+++ DE0_TOP/DE0_TOP.v
@@ -18,58 +18,6 @@ module DE0_TOP
            output HEX3_DP,
            //////////////////////// LED ////////////////////////
            output [ 9: 0 ] LEDG,
-           //////////////////////// UART ////////////////////////
-           output UART_TXD,
-           input UART_RXD,
-           output UART_CTS,
-           input UART_RTS,
-           //////////////////////// SDRAM Interface ////////////////////////
-           inout [ 15: 0 ] DRAM_DQ,
-           output [ 12: 0 ] DRAM_ADDR,
-           output DRAM_LDQM,
-           output DRAM_UDQM,
-           output DRAM_WE_N,
-           output DRAM_CAS_N,
-           output DRAM_RAS_N,
-           output DRAM_CS_N,
-           output DRAM_BA_0,
-           output DRAM_BA_1,
-           output DRAM_CLK,
-           output DRAM_CKE,
-           //////////////////////// Flash Interface ////////////////////////
-           inout [ 14: 0 ] FL_DQ,
-           inout FL_DQ15_AM1,
-           output [ 21: 0 ] FL_ADDR,
-           output FL_WE_N,
-           output FL_RST_N,
-           output FL_OE_N,
-           output FL_CE_N,
-           output FL_WP_N,
-           output FL_BYTE_N,
-           input FL_RY,
-           //////////////////////// LCD Module 16X2 ////////////////////////
-           inout [ 7: 0 ] LCD_DATA,
-           output LCD_BLON,
-           output LCD_RW,
-           output LCD_EN,
-           output LCD_RS,
-           //////////////////////// SD Card Interface ////////////////////////
-           inout SD_DAT0,
-           inout SD_DAT3,
-           inout SD_CMD,
-           output SD_CLK,
-           input SD_WP_N,
-           //////////////////////// PS2 ////////////////////////
-           inout PS2_KBDAT,
-           inout PS2_KBCLK,
-           inout PS2_MSDAT,
-           inout PS2_MSCLK,
-           //////////////////////// VGA ////////////////////////
-           output VGA_HS,
-           output VGA_VS,
-           output [ 3: 0 ] VGA_R,
-           output [ 3: 0 ] VGA_G,
-           output [ 3: 0 ] VGA_B,
            //////////////////////// GPIO ////////////////////////
            input [ 1: 0 ] GPIO0_CLKIN,
            output [ 1: 0 ] GPIO0_CLKOUT,
@@ -82,17 +30,12 @@ module DE0_TOP
 // =======================================================
 // REG/WIRE declarations
 // =======================================================
-// All inout port turn to tri-state
-assign DRAM_DQ = 16'hzzzz;
-assign FL_DQ = 16'hzzzz;
-assign LCD_DATA = 8'hzz;
-assign SD_DAT0 = 1'hz;
-assign SD_DAT3 = 1'hz;
-assign SD_CMD = 1'hz;
-assign PS2_KBDAT = 1'hz;
-assign PS2_KBCLK = 1'hz;
-assign PS2_MSDAT = 1'hz;
-assign PS2_MSCLK = 1'hz;
+
+
+//=======================================================
+// Input/Output assignments
+//=======================================================
+// All unused inout port turn to tri-state
 assign GPIO0_D = 32'hzzzzzzzz;
 assign GPIO1_D = 32'hzzzzzzzz;
 
